aa1ee9d2ed19ffdca3ad68a890404dda376709b2
bolosmihai1988 <emailul_tu@example.com>
2025-11-25 23:24:23 +0200

    Added Deviz API controller and PDF generation fix

--- Controllers/DevizController.cs
+++ Controllers/DevizController.cs
@@ -19,6 +19,7 @@ namespace DevizWebApp.Controllers
         }
 
         [HttpPost]
+        [IgnoreAntiforgeryToken]
         public IActionResult GeneratePDF(DevizDocumentModel model)
         {
             if (model == null)
